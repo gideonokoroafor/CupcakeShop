02ad80cf6844d7f2cc0e548b70c253be3acba531
gideonokoroafor <okoroaforgid@gmail.com>
2024-11-29 11:50:24 -0600

    Initial Commit

---
+++ CupcakeShop/CupcakeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeShopApp.swift
+//  CupcakeShop
+//
+//  Created by Gideon Okoroafor on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
